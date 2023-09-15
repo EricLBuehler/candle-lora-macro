9d3ee8a932624d304427942071339c3b01d0a70e
EricLBuehler <ericlbuehler@gmail.com>
2023-09-15 07:52:25 -0400

    Implement for Option<T>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream as TokenStream1;
 use proc_macro2::TokenStream;
 use syn::{
-    parse::Parser, parse_macro_input, Data, DeriveInput, Fields, GenericArgument, Ident, Type,
-    TypeParamBound, Visibility,
+    parse::Parser, parse_macro_input, Data, DeriveInput, Fields, GenericArgument, Ident,
+    PathArguments, Type, TypeParamBound, Visibility,
 };
 
 #[proc_macro_attribute]
@@ -55,6 +55,67 @@ pub fn replace_layer_fields(_args: TokenStream1, input: TokenStream1) -> TokenSt
                                             f = Some(syn::Field::parse_named.parse2(quote::quote!(#ident: Box<dyn EmbeddingLayerLike>)).unwrap());
                                         }
                                     }
+                                    "Option" => {
+                                        if let PathArguments::AngleBracketed(bracketed) =
+                                            &path.path.segments.first().unwrap().arguments
+                                        {
+                                            if bracketed.args.len() == 1 {
+                                                if let GenericArgument::Type(Type::Path(tp)) =
+                                                    bracketed.args.first().unwrap()
+                                                {
+                                                    if tp.path.segments.len() == 1 {
+                                                        match tp
+                                                            .path
+                                                            .segments
+                                                            .first()
+                                                            .unwrap()
+                                                            .ident
+                                                            .to_string()
+                                                            .as_str()
+                                                        {
+                                                            "Linear" => {
+                                                                if let Visibility::Public(_) =
+                                                                    field.vis
+                                                                {
+                                                                    f = Some(syn::Field::parse_named.parse2(quote::quote!(pub #ident: Option<Box<dyn LinearLayerLike>>)).unwrap());
+                                                                } else {
+                                                                    f = Some(syn::Field::parse_named.parse2(quote::quote!(#ident: Option<Box<dyn LinearLayerLike>>)).unwrap());
+                                                                }
+                                                            }
+                                                            "Conv1d" => {
+                                                                if let Visibility::Public(_) =
+                                                                    field.vis
+                                                                {
+                                                                    f = Some(syn::Field::parse_named.parse2(quote::quote!(pub #ident: Option<Box<dyn Conv1dLayerLike>>)).unwrap());
+                                                                } else {
+                                                                    f = Some(syn::Field::parse_named.parse2(quote::quote!(#ident: Option<Box<dyn Conv1dLayerLike>>)).unwrap());
+                                                                }
+                                                            }
+                                                            "Conv2d" => {
+                                                                if let Visibility::Public(_) =
+                                                                    field.vis
+                                                                {
+                                                                    f = Some(syn::Field::parse_named.parse2(quote::quote!(pub #ident: Option<Box<dyn Conv2dLayerLike>>)).unwrap());
+                                                                } else {
+                                                                    f = Some(syn::Field::parse_named.parse2(quote::quote!(#ident: Option<Box<dyn Conv2dLayerLike>>)).unwrap());
+                                                                }
+                                                            }
+                                                            "Embedding" => {
+                                                                if let Visibility::Public(_) =
+                                                                    field.vis
+                                                                {
+                                                                    f = Some(syn::Field::parse_named.parse2(quote::quote!(pub #ident: Option<Box<dyn EmbeddingLayerLike>>)).unwrap());
+                                                                } else {
+                                                                    f = Some(syn::Field::parse_named.parse2(quote::quote!(#ident: Option<Box<dyn EmbeddingLayerLike>>)).unwrap());
+                                                                }
+                                                            }
+                                                            _ => {}
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
                                     _ => {}
                                 }
                             } else {
@@ -93,6 +154,11 @@ pub fn auto_lora_convert(tokens: TokenStream1) -> TokenStream1 {
     let mut conv1d_fields = Vec::new();
     let mut conv2d_fields = Vec::new();
     let mut embed_fields = Vec::new();
+
+    let mut linear_option1_fields = Vec::new();
+    let mut conv1d_option1_fields = Vec::new();
+    let mut conv2d_option1_fields = Vec::new();
+    let mut embed_option1_fields = Vec::new();
     let st_name = &ast.ident;
 
     match ast.data {
@@ -102,6 +168,82 @@ pub fn auto_lora_convert(tokens: TokenStream1) -> TokenStream1 {
                     Type::Path(path) => {
                         let segments = path.path.segments.into_iter().collect::<Vec<_>>();
                         if segments.len() != 1 {
+                            continue;
+                        }
+                        if is_ident(&segments[0].ident, "Option") {
+                            if let syn::PathArguments::AngleBracketed(bracketed) =
+                                &segments.get(0).as_ref().unwrap().arguments
+                            {
+                                if bracketed.args.len() != 1 {
+                                    continue;
+                                }
+                                if let GenericArgument::Type(Type::Path(ty)) = &bracketed.args[0] {
+                                    if ty.path.segments.len() != 1 {
+                                        continue;
+                                    }
+                                    let typname = &ty.path.segments.first().unwrap().ident;
+                                    if is_ident(typname, "Box") {
+                                        if let syn::PathArguments::AngleBracketed(bracketed) =
+                                            &ty.path.segments.first().as_ref().unwrap().arguments
+                                        {
+                                            if bracketed.args.len() != 1 {
+                                                continue;
+                                            }
+                                            match &bracketed.args[0] {
+                                                GenericArgument::Type(Type::TraitObject(trobj)) => {
+                                                    let bounds = &trobj.bounds;
+                                                    if bounds.len() != 1 {
+                                                        continue;
+                                                    }
+                                                    match bounds.first().unwrap() {
+                                                        TypeParamBound::Trait(bound) => {
+                                                            if bound.path.segments.len() != 1 {
+                                                                continue;
+                                                            }
+                                                            let trt = &bound
+                                                                .path
+                                                                .segments
+                                                                .first()
+                                                                .unwrap()
+                                                                .ident;
+                                                            let value = (
+                                                                field.ident.clone().unwrap(),
+                                                                field
+                                                                    .ident
+                                                                    .as_ref()
+                                                                    .unwrap()
+                                                                    .to_string(),
+                                                            );
+                                                            if is_ident(trt, "LinearLayerLike") {
+                                                                linear_option1_fields.push(value);
+                                                            } else if is_ident(
+                                                                trt,
+                                                                "Conv1dLayerLike",
+                                                            ) {
+                                                                conv1d_option1_fields.push(value);
+                                                            } else if is_ident(
+                                                                trt,
+                                                                "Conv2dLayerLike",
+                                                            ) {
+                                                                conv2d_option1_fields.push(value);
+                                                            } else if is_ident(
+                                                                trt,
+                                                                "EmbeddingLayerLike",
+                                                            ) {
+                                                                embed_option1_fields.push(value);
+                                                            }
+                                                        }
+                                                        _ => continue,
+                                                    }
+                                                }
+                                                _ => continue,
+                                            }
+                                        } else {
+                                            continue;
+                                        }
+                                    }
+                                }
+                            }
                             continue;
                         }
                         if !is_ident(&segments[0].ident, "Box") {
@@ -157,13 +299,11 @@ pub fn auto_lora_convert(tokens: TokenStream1) -> TokenStream1 {
         }
     }
 
-    //let st_name = Ident::new("Model", Span::mixed_site());
-
     let mut linear_stream = TokenStream::new();
     if !linear_fields.is_empty() {
         quote_into::quote_into!(linear_stream += [#{
-            for (_,name) in linear_fields.iter() {
-                quote_into::quote_into!(linear_stream += (linear.insert(#name.to_string(), &*self.a)),)
+            for (namei,name) in linear_fields.iter() {
+                quote_into::quote_into!(linear_stream += (linear.insert(#name.to_string(), &*self.#namei)),)
             }
         }];);
     }
@@ -171,8 +311,8 @@ pub fn auto_lora_convert(tokens: TokenStream1) -> TokenStream1 {
     let mut conv1d_stream = TokenStream::new();
     if !conv1d_fields.is_empty() {
         quote_into::quote_into!(conv1d_stream += [#{
-            for (_,name) in conv1d_fields.iter() {
-                quote_into::quote_into!(conv1d_stream += (linear.insert(#name.to_string(), &*self.a)),)
+            for (namei,name) in conv1d_fields.iter() {
+                quote_into::quote_into!(conv1d_stream += (linear.insert(#name.to_string(), &*self.#namei)),)
             }
         }];);
     }
@@ -180,8 +320,8 @@ pub fn auto_lora_convert(tokens: TokenStream1) -> TokenStream1 {
     let mut conv2d_stream = TokenStream::new();
     if !conv2d_fields.is_empty() {
         quote_into::quote_into!(conv2d_stream += [#{
-            for (_,name) in conv2d_fields.iter() {
-                quote_into::quote_into!(conv2d_stream += (linear.insert(#name.to_string(), &*self.a)),)
+            for (namei,name) in conv2d_fields.iter() {
+                quote_into::quote_into!(conv2d_stream += (linear.insert(#name.to_string(), &*self.#namei)),)
             }
         }];);
     }
@@ -189,8 +329,8 @@ pub fn auto_lora_convert(tokens: TokenStream1) -> TokenStream1 {
     let mut embed_stream = TokenStream::new();
     if !embed_fields.is_empty() {
         quote_into::quote_into!(embed_stream += [#{
-            for (_,name) in embed_fields.iter() {
-                quote_into::quote_into!(embed_stream += (linear.insert(#name.to_string(), &*self.a)),)
+            for (namei,name) in embed_fields.iter() {
+                quote_into::quote_into!(embed_stream += (linear.insert(#name.to_string(), &*self.#namei)),)
             }
         }];);
     }
@@ -231,6 +371,78 @@ pub fn auto_lora_convert(tokens: TokenStream1) -> TokenStream1 {
         }];);
     }
 
+    let mut linear_option1_stream = TokenStream::new();
+    if !linear_option1_fields.is_empty() {
+        quote_into::quote_into!(linear_option1_stream += [#{
+            for (namei,name) in linear_option1_fields.iter() {
+                quote_into::quote_into!(linear_option1_stream += (linear.insert(#name.to_string(), self.#namei.as_deref().unwrap())),)
+            }
+        }];);
+    }
+
+    let mut conv1d_option1_stream = TokenStream::new();
+    if !conv1d_option1_fields.is_empty() {
+        quote_into::quote_into!(conv1d_option1_stream += [#{
+            for (namei,name) in conv1d_option1_fields.iter() {
+                quote_into::quote_into!(conv1d_option1_stream += (linear.insert(#name.to_string(), &*self.#namei)),)
+            }
+        }];);
+    }
+
+    let mut conv2d_option1_stream = TokenStream::new();
+    if !conv2d_option1_fields.is_empty() {
+        quote_into::quote_into!(conv2d_option1_stream += [#{
+            for (namei,name) in conv2d_option1_fields.iter() {
+                quote_into::quote_into!(conv2d_option1_stream += (linear.insert(#name.to_string(), &*self.#namei)),)
+            }
+        }];);
+    }
+
+    let mut embed_option1_stream = TokenStream::new();
+    if !embed_option1_fields.is_empty() {
+        quote_into::quote_into!(embed_option1_stream += [#{
+            for (namei,name) in embed_option1_fields.iter() {
+                quote_into::quote_into!(embed_option1_stream += (linear.insert(#name.to_string(), &*self.#namei)),)
+            }
+        }];);
+    }
+
+    let mut linear_option1_stream_assign = TokenStream::new();
+    if !linear_option1_fields.is_empty() {
+        quote_into::quote_into!(linear_option1_stream_assign += [#{
+            for (name, n) in linear_option1_fields {
+                linear_option1_stream_assign.extend(quote::quote!((self.#name = ::std::option::Option::Some(::std::boxed::Box::new(new_layers.linear.get(#n).unwrap().clone()))),))
+            }
+        }];);
+    }
+
+    let mut conv1d_option1_stream_assign = TokenStream::new();
+    if !conv1d_option1_fields.is_empty() {
+        quote_into::quote_into!(conv1d_option1_stream_assign += [#{
+            for (name, n) in conv1d_option1_fields {
+                conv1d_option1_stream_assign.extend(quote::quote!((self.#name = ::std::option::Option::Some(::std::boxed::Box::new(new_layers.conv1d.get(#n).unwrap().clone()))),))
+            }
+        }];);
+    }
+
+    let mut conv2d_option1_stream_assign = TokenStream::new();
+    if !conv2d_option1_fields.is_empty() {
+        quote_into::quote_into!(conv2d_option1_stream_assign += [#{
+            for (name, n) in conv2d_option1_fields {
+                conv2d_option1_stream_assign.extend(quote::quote!((self.#name = ::std::option::Option::Some(::std::boxed::Box::new(new_layers.conv2d.get(#n).unwrap().clone()))),))
+            }
+        }];);
+    }
+
+    let mut embed_option1_stream_assign = TokenStream::new();
+    if !embed_option1_fields.is_empty() {
+        quote_into::quote_into!(embed_option1_stream_assign += [#{
+            for (name, n) in embed_option1_fields {
+                embed_option1_stream_assign.extend(quote::quote!((self.#name = ::std::option::Option::Some(::std::boxed::Box::new(new_layers.embed.get(#n).unwrap().clone()))),))
+            }
+        }];);
+    }
+
     let mut stream = TokenStream::new();
     quote_into::quote_into! { stream +=
         impl #st_name {
@@ -244,6 +456,11 @@ pub fn auto_lora_convert(tokens: TokenStream1) -> TokenStream1 {
                 #conv1d_stream
                 #conv2d_stream
                 #embed_stream
+
+                #linear_option1_stream
+                #conv1d_option1_stream
+                #conv2d_option1_stream
+                #embed_option1_stream
 
                 let mut builder = candle_lora::SelectedLayersBuilder::new();
                 if linear_config.is_some() {
@@ -266,6 +483,11 @@ pub fn auto_lora_convert(tokens: TokenStream1) -> TokenStream1 {
                 #conv1d_stream_assign
                 #conv2d_stream_assign
                 #embed_stream_assign
+
+                #linear_option1_stream_assign
+                #conv1d_option1_stream_assign
+                #conv2d_option1_stream_assign
+                #embed_option1_stream_assign
             }
         }
     }
